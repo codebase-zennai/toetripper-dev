f28f06d4a28f7e6fc12bde6183f172f0d4fff6fd
hevardhan <paatukkorupulavan@gmail.com>
2026-06-03 01:54:11 +0530

    Images fixed

--- app/services/corporate-travels/components/ServicesHero.js
+++ app/services/corporate-travels/components/ServicesHero.js
@@ -74,36 +74,17 @@ export default function ServicesHero() {
         data-w-id="26f29636-e130-073f-81a3-5680a2a67ade"
         className="w-layout-blockcontainer services-container rounded-corners w-container"
       >
-        <div
-          data-poster-url="https://cdn.prod.website-files.com/66e3df8d47eb3991ca9dbef7%2F66e5500c4a3c1a3ac366692f_7678101-hd_1920_1080_25fps-poster-00001.jpg"
-          data-video-urls="https://cdn.prod.website-files.com/66e3df8d47eb3991ca9dbef7%2F66e5500c4a3c1a3ac366692f_7678101-hd_1920_1080_25fps-transcode.mp4,https://cdn.prod.website-files.com/66e3df8d47eb3991ca9dbef7%2F66e5500c4a3c1a3ac366692f_7678101-hd_1920_1080_25fps-transcode.webm"
-          data-autoplay="true"
-          data-loop="true"
-          data-wf-ignore="true"
-          className="services-video w-background-video w-background-video-atom"
-        >
-          <video
-            id="29034c61-51ff-6df6-071e-f08735a7a07a-video"
-            autoPlay=""
-            loop=""
+        <div className="services-video">
+          <img
+            src="/images/corporate_travel_hero.png"
+            alt="Corporate Travel Management"
             style={{
-              backgroundImage:
-                "url('https://cdn.prod.website-files.com/66e3df8d47eb3991ca9dbef7%2F66e5500c4a3c1a3ac366692f_7678101-hd_1920_1080_25fps-poster-00001.jpg')",
+              width: '100%',
+              height: '100%',
+              objectFit: 'cover',
+              display: 'block'
             }}
-            muted={true}
-            playsInline=""
-            data-wf-ignore="true"
-            data-object-fit="cover"
-          >
-            <source
-              src="https://cdn.prod.website-files.com/66e3df8d47eb3991ca9dbef7%2F66e5500c4a3c1a3ac366692f_7678101-hd_1920_1080_25fps-transcode.mp4"
-              data-wf-ignore="true"
-            />
-            <source
-              src="https://cdn.prod.website-files.com/66e3df8d47eb3991ca9dbef7%2F66e5500c4a3c1a3ac366692f_7678101-hd_1920_1080_25fps-transcode.webm"
-              data-wf-ignore="true"
-            />
-          </video>
+          />
         </div>
       </div>
     </div>
